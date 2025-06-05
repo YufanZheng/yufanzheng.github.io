47fd9d1cbfa20c580014ba15aff22b81b99bb3ab
Yufan Zheng (郑煜钒) <zhjpre@gmail.com>
2025-06-05 22:26:03 +0800

    update

--- src/uploader.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react'
-import OneDriveApi from '@harrisoff/onedrive-js-sdk'
-
-import { useAccessToken } from './utils';
-
-export const useOneDriveClient = () => {
-  const accessToken = useAccessToken() || ''
-  return useMemo(() => new OneDriveApi({ accessToken }), [accessToken])
-}
